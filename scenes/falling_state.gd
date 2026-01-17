371bc6f65e1ce8aa09f26a238aea32c2ae2c404f
Myrty <derusebastien@gmail.com>
2026-01-17 08:57:33 +0100

    add fall animation

--- scenes/falling_state.gd
+++ scenes/falling_state.gd
@@ -1,14 +1,20 @@
 extends PlayerState
 
 func enter(previous_state_path: String, data := {}) -> void:
-	#animated_sprite_2d.play("fall")
-	pass
+	animated_sprite_2d.play("fall")
 
 		
 func physics_update(delta: float) -> void:
 	var input_direction_x := Input.get_axis("left", "right")
 	player.velocity.x = player.SPEED * input_direction_x
 	player.velocity.y += player.get_gravity().y * delta
+	
+	if input_direction_x:
+		if input_direction_x > 0:
+			animated_sprite_2d.flip_h = true
+		else:
+			animated_sprite_2d.flip_h = false
+	
 	player.move_and_slide()
 
 	if player.is_on_floor():
--- scenes/jumping_state.gd
+++ scenes/jumping_state.gd
@@ -10,6 +10,13 @@ func physics_update(_delta: float) -> void:
 	var input_direction_x := Input.get_axis("left", "right")
 	player.velocity.x = player.SPEED * input_direction_x
 	player.velocity.y += player.get_gravity().y * _delta
+	
+	if input_direction_x:
+		if input_direction_x > 0:
+			animated_sprite_2d.flip_h = true
+		else:
+			animated_sprite_2d.flip_h = false
+	
 	player.move_and_slide()
 	
 
