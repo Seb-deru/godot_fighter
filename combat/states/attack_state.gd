1ce8916a3679b08a55eb3cd400bf80257d78ed22
Seb Deru <109200015+Seb-deru@users.noreply.github.com>
2026-01-23 15:11:09 +0100

    wip frame based attack

--- combat/states/attack_state.gd
+++ combat/states/attack_state.gd
@@ -1,11 +1,25 @@
 extends PlayerState
 
+var frame := 0
+var move : MoveData
+
 func enter(previous_state_path: String, data: MoveData = null) -> void:
+	move = data
 	if animated_player.has_animation(data.animation):
 		animated_player.play(data.animation)
-	else:
-		animated_player.stop()
 
-func physics_update(_delta: float) -> void:
-	if not animated_player.is_playing():
-		finished.emit(IDLE, null)
+func physics_update(delta: float) -> void:
+	frame += 1
+	
+	if frame == move.startup:
+		#enable_hitboxes()
+		pass
+	elif frame == move.startup + move.active:
+		#disable_hitboxes()
+		pass
+	elif frame >= move.startup + move.active + move.recovery:
+		exit_to_idle()
+
+func exit_to_idle()-> void:
+	frame = 0
+	finished.emit(IDLE, null)
