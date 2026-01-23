c121c08ac77156151f6fdf8b07a43005f4cdb211
Seb Deru <109200015+Seb-deru@users.noreply.github.com>
2026-01-23 15:02:22 +0100

    fix missing attack animation

--- combat/states/attack_state.gd
+++ combat/states/attack_state.gd
@@ -1,7 +1,10 @@
 extends PlayerState
 
 func enter(previous_state_path: String, data: MoveData = null) -> void:
-	animated_player.play(data.animation)
+	if animated_player.has_animation(data.animation):
+		animated_player.play(data.animation)
+	else:
+		animated_player.stop()
 
 func physics_update(_delta: float) -> void:
 	if not animated_player.is_playing():
